8ae568c58382a67f835b9d981170366f9e61431d
Jamie Dixon <jamessdixon@gmail.com>
2016-07-03 22:05:05 -0400

    Added new projects

---
+++ source/ChickenSoftware.SomeInsuranceCompany.Solution/ChickenSoftware.Processor.UI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+using ChickenSoftware.CustomerProcessor;
+
+namespace ChickenSoftware.Processor.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting....");
+
+            string connectionString = ConfigurationManager.ConnectionStrings[0].ConnectionString;
+
+            string[] phoneNumbers = new string[] { "919-555-1212", "919-867-5309" };
+            Guid licenseKey = Guid.NewGuid();
+            var notifier = new CDyneTextNotifier(phoneNumbers,licenseKey);
+
+
+            var processor = new CustomerProcessor(connectionString, notifier);
+            Console.WriteLine("Ending...");
+            Console.ReadKey();
+        }
+    }
+}
